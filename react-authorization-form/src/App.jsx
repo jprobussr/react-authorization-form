d1a507a3314dc4973abc44ea0a4f0f99dc0bd65a
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-04-15 17:14:51 -0500

    chore: initialize Vite React app and clean starter files

---
+++ react-authorization-form/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <h1>Authorization Form</h1>
+      <p>React project setup is ready</p>
+    </main>
+  )
+}
+
+export default App
